a84d2343ebc8559c4039e7c1177181b4312558c7
Balazs Bamer <balazs@bamer.hu>
2021-01-22 18:51:51 +0100

    Added CLion project.

--- src/main-node.cpp
+++ src/main-node.cpp
@@ -4,14 +4,14 @@
 class NodeLinkIntersectionBruteForce : public rclcpp::Node {
 public:
   NodeLinkIntersectionBruteForce() : Node("linkIntersectionBruteForce") {
-    RCLCPP_INFO(get_logger(), "log works");
+    RCLCPP_INFO(get_logger(), "log still works");
   }
 };
 
 int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
   auto node = std::make_shared<NodeLinkIntersectionBruteForce>();
-  rclcpp::spin(node);
+  rclcpp::spin_some(node);
   rclcpp::shutdown();
   return 0;
 }

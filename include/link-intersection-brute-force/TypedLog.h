8f5d1756663cd5dae95a0f2a37e361318717f66f
Balazs Bamer <balazs@bamer.hu>
2021-01-31 19:34:55 +0100

    Switched to own typed logger.

---
+++ include/link-intersection-brute-force/TypedLog.h
@@ -0,0 +1,52 @@
+//
+// Created by balazs on 2021. 01. 31..
+//
+
+#ifndef LINK_INTERSECTION_BRUTE_FORCE_TYPEDLOG_H
+#define LINK_INTERSECTION_BRUTE_FORCE_TYPEDLOG_H
+
+#include "LogAppInterfaceStd.h"
+#include "LogConverterCustomText.h"
+#include "LogSenderRos2.h"
+#include "LogQueueStdBoost.h"
+#include "LogMessageCompact.h"
+#include "Log.h"
+
+namespace LogTopics {
+  nowtech::log::TopicInstance system;
+  nowtech::log::TopicInstance dumpUrdf;
+}
+
+constexpr nowtech::log::TaskId cgMaxTaskCount = 23;
+constexpr bool cgAllowRegistrationLog = true;
+constexpr bool cgLogFromIsr = false;
+constexpr size_t cgTaskShutdownSleepPeriod = 100u;
+constexpr bool cgArchitecture64 = true;
+constexpr uint8_t cgAppendStackBufferSize = 100u;
+constexpr bool cgAppendBasePrefix = true;
+constexpr bool cgAlignSigned = false;
+using AtomicBufferType = int32_t;
+constexpr size_t cgAtomicBufferExponent = 14u;
+constexpr AtomicBufferType cgAtomicBufferInvalidValue = 1234546789;
+constexpr size_t cgTransmitBufferSize = 65536u;
+constexpr size_t cgPayloadSize = 32u;
+constexpr bool cgSupportFloatingPoint = true;
+constexpr size_t cgQueueSize = 4444u;
+constexpr nowtech::log::LogTopic cgMaxTopicCount = 2;
+constexpr nowtech::log::TaskRepresentation cgTaskRepresentation = nowtech::log::TaskRepresentation::cName;
+constexpr size_t cgDirectBufferSize = 0u;
+constexpr nowtech::log::ErrorLevel cgErrorLevel = nowtech::log::ErrorLevel::Info;
+
+using LogAppInterface = nowtech::log::AppInterfaceStd<cgMaxTaskCount, cgLogFromIsr, cgTaskShutdownSleepPeriod>;
+constexpr typename LogAppInterface::LogTime cgTimeout = 123u;
+constexpr typename LogAppInterface::LogTime cgRefreshPeriod = 444;
+constexpr int cgSimulatedRos2Loglevel = 50;
+using LogMessage = nowtech::log::MessageCompact<cgPayloadSize, cgSupportFloatingPoint>;
+using LogConverter = nowtech::log::ConverterCustomText<LogMessage, cgArchitecture64, cgAppendStackBufferSize, cgAppendBasePrefix, cgAlignSigned>;
+using LogSender = nowtech::log::SenderRos2<LogAppInterface, LogConverter, cgTransmitBufferSize, cgTimeout, cgSimulatedRos2Loglevel>;
+using LogQueue = nowtech::log::QueueStdBoost<LogMessage, LogAppInterface, cgQueueSize>;
+using LogAtomicBuffer = nowtech::log::AtomicBufferOperational<LogAppInterface, AtomicBufferType, cgAtomicBufferExponent, cgAtomicBufferInvalidValue>;
+using LogConfig = nowtech::log::Config<cgAllowRegistrationLog, cgMaxTopicCount, cgTaskRepresentation, cgDirectBufferSize, cgRefreshPeriod, cgErrorLevel>;
+using Log = nowtech::log::Log<LogQueue, LogSender, LogAtomicBuffer, LogConfig>;
+
+#endif //LINK_INTERSECTION_BRUTE_FORCE_TYPEDLOG_H
--- src/main-node.cpp
+++ src/main-node.cpp
@@ -1,25 +1,107 @@
 //#include <urdf/model.h>
+
+#include "TypedLog.h"
 #include "rclcpp/rclcpp.hpp"
 #include "urdf/model.h"
 #include <string>
+#include <map>
 
 class NodeLinkIntersectionBruteForce final : public rclcpp::Node {
 private:
   static constexpr int csUrdfArgumentIndex = 1;
 
   urdf::Model mModel;
+  std::map<std::string, std::string> mParentLinkTree;
+  static constexpr char csJointTypeNames[][16u] = {
+    "UNKNOWN", "REVOLUTE", "CONTINUOUS", "PRISMATIC", "FLOATING", "PLANAR", "FIXED"
+  };
 
 public:
-  NodeLinkIntersectionBruteForce(int aArgc, char** aArgv) : Node("linkIntersectionBruteForce") {
+  NodeLinkIntersectionBruteForce(int, char** aArgv) : Node("linkIntersectionBruteForce") {
     mModel.initFile(std::string{aArgv[csUrdfArgumentIndex]});
-    RCLCPP_INFO(get_logger(), mModel.getName());
+    Log::i(LogTopics::dumpUrdf) << mModel.getName() << Log::end;
+    mModel.initTree(mParentLinkTree);
+    mModel.initRoot(mParentLinkTree);
+    Log::i(LogTopics::dumpUrdf) << mModel.getRoot()->name << Log::end;
+    dumpInfo(mModel.getRoot());
+    for(auto &item : mParentLinkTree) {
+      Log::i(LogTopics::dumpUrdf) << item.second << " -> " << item.first << Log::end;
+      auto link = mModel.getLink(item.first);
+      dumpInfo(link);
+      dumpInfo(link->parent_joint);
+      Log::i(LogTopics::dumpUrdf) << Log::end;
+    }
+  }
+
+private:
+  rclcpp::Logger l() const {
+    return get_logger();
+  }
+
+  void dumpInfo(urdf::LinkConstSharedPtr aLink) {
+    Log::i(LogTopics::dumpUrdf) << "---link---" << Log::end;
+    if(aLink->collision) {
+      if(aLink->collision->geometry->type != urdf::Geometry::MESH) {
+        throw std::invalid_argument("Only meshes are supported as geometry.");
+      }
+      else { // nothing to do
+      }
+      auto coll = aLink->collision.get();
+      auto geomsh{coll->geometry};
+      auto geom = geomsh.get();
+      auto mesh = dynamic_cast<urdf::Mesh*>(geom);
+      Log::i(LogTopics::dumpUrdf) << "mesh: x:" << mesh->scale.x << " y:" << mesh->scale.y << " z:" << mesh->scale.z << " filename: " << mesh->filename << Log::end;
+      dumpInfo(aLink->collision->origin);
+    }
+    else {
+      Log::i(LogTopics::dumpUrdf) << "no collision" << Log::end;
+    }
+    Log::i(LogTopics::dumpUrdf) << Log::end;
+  }
+
+  void dumpInfo(urdf::JointConstSharedPtr aJoint) {
+    Log::i(LogTopics::dumpUrdf) << "---joint---" << Log::end;
+    Log::i(LogTopics::dumpUrdf) << csJointTypeNames[aJoint->type] << Log::end;
+    if(aJoint->type != urdf::Joint::REVOLUTE
+    && aJoint->type != urdf::Joint::CONTINUOUS
+    && aJoint->type != urdf::Joint::PRISMATIC
+    && aJoint->type != urdf::Joint::FIXED) {
+      throw std::invalid_argument("Only fixed, revolute and prismatic joints are supported");
+    }
+    else { // nothing to do
+    }
+    Log::i(LogTopics::dumpUrdf) << "axis x:" << aJoint->axis.x << " y:" << aJoint->axis.y << " z:" << aJoint->axis.z << Log::end;
+    dumpInfo(aJoint->parent_to_joint_origin_transform);
+    if(aJoint->limits) {
+      Log::i(LogTopics::dumpUrdf) << "limit l:" << aJoint->limits->lower << " u:" << aJoint->limits->upper << Log::end;
+    }
+    else {
+      Log::i(LogTopics::dumpUrdf) << "no limit" << Log::end;
+    }
+  }
+
+  void dumpInfo(urdf::Pose const &aPose) {
+    Log::i(LogTopics::dumpUrdf) << "origin rotation w:" << aPose.rotation.w << " x:" << aPose.rotation.x << " y:" << aPose.rotation.y << " z:" << aPose.rotation.z << Log::end;
+    Log::i(LogTopics::dumpUrdf) << "origin position x:" << aPose.position.x << " y:" << aPose.position.y << " z:" << aPose.position.z << Log::end;
   }
 };
 
+// args as in launch:
+// /home/balazs/munka/cuda-trajectory-planner/ros-workspace/install/link-intersection-brute-force/share/link-intersection-brute-force/px150_coll.urdf --ros-args -r __node:=linkIntersectionBruteForce -r __ns:=/cudaTrajectoryPlanner
 int main(int aArgc, char **aArgv) {
   rclcpp::init(aArgc, aArgv);
+  nowtech::log::LogFormatConfig logConfig;
+  LogSender::init();
+  Log::init(logConfig);
+  Log::registerTopic(LogTopics::system, "system");
+  Log::registerTopic(LogTopics::dumpUrdf, "dumpUrdf");
+  Log::registerCurrentTask("main");
   auto node = std::make_shared<NodeLinkIntersectionBruteForce>(aArgc, aArgv);
   rclcpp::spin_some(node);
+  Log::unregisterCurrentTask();
+  Log::done();
   rclcpp::shutdown();
   return 0;
 }
+
+constexpr char NodeLinkIntersectionBruteForce::csJointTypeNames[][16u];

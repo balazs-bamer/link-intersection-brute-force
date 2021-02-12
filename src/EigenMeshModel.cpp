1b8064e2e02974d9783e6d5abc320bb5e6bf5a27
Balazs Bamer <balazs@bamer.hu>
2021-02-12 12:29:09 +0100

    Some progress.

--- src/EigenMeshModel.cpp
+++ src/EigenMeshModel.cpp
@@ -6,12 +6,13 @@
 #include "TypedLog.h"
 #include <list>
 #include <stdexcept>
+#include <cstring>
 
 fragor::EigenMeshJointLink::EigenMeshJointLink(urdf::Model const &aModel,
                                                Id const aActualLocalRootId,
                                                std::unordered_map<Id, Id> const &aLinkId2parentLinkId,
                                                std::unordered_set<Id> const &aCurrentLimbIds,
-                                               std::unordered_map<Id, std::string> const aId2name)
+                                               std::unordered_map<Id, std::string> const &aId2name)
                                                : mActualLocalRootId(aActualLocalRootId)
                                                , mFirstLink(aModel.getLink(aId2name.at(aActualLocalRootId)))
                                                , mJoint(mFirstLink->parent_joint) {
@@ -23,21 +24,34 @@ fragor::EigenMeshJointLink::EigenMeshJointLink(urdf::Model const &aModel,
     auto actualLinkId = *(links2process.begin());
     links2process.pop_front();
     auto coll = aModel.getLink(aId2name.at(actualLinkId))->collision.get();
-    auto geomsh{coll->geometry}; // TODO coll is null sometimes
-    auto geom = geomsh.get();
-    auto mesh = dynamic_cast<urdf::Mesh*>(geom);
-    if(mesh != nullptr) {
-      Log::n() << mesh->filename << Log::end;
-    }
-    else {
-      // TODO something should happen to links without a mesh.
-    }
-    for(auto &i : aLinkId2parentLinkId) {
-      if(i.second == actualLinkId && remainingLinks.contains(i.first)) {
-        remainingLinks.erase(i.first);
-        links2process.push_back(i.first);
+    std::string filename;
+    if(coll != nullptr) {
+      auto geomSh{coll->geometry};
+      auto mesh = dynamic_cast<urdf::Mesh *>(geomSh.get());
+      if (mesh != nullptr) {
+        filename = mesh->filename;
+        if(filename.starts_with(csStlNamePrefix)) {
+          filename.erase(0u, std::strlen(csStlNamePrefix));
+        }
+        else { // nothing to do
+        }
+        Log::n() << aId2name.at(actualLinkId) << filename << Log::end;
+      } else {
+        Log::n() << aId2name.at(actualLinkId) << "NONE" << Log::end;
       }
-      else { // nothing to do
+    }
+    else { // TODO check how to carry on with transformation chain
+    }
+    for (auto &i : aLinkId2parentLinkId) {
+      if (i.second == actualLinkId) {
+        if(remainingLinks.contains(i.first)) {
+          remainingLinks.erase(i.first);
+          links2process.push_back(i.first);
+        }
+        else {  // Found a child with non-fixed joint.
+          // TODO process
+        }
+      } else { // nothing to do
       }
     }
   }
@@ -80,22 +94,22 @@ fragor::EigenMeshModel::EigenMeshModel(urdf::Model const &aModel, std::map<std::
     bool wasIncrement;
     do {
       wasIncrement = false;
-      for (auto &i : linkId2parentLinkId) {                // O(n^2) but who cares? Not many links in an URDF.
-        if(currentLimbIds.contains(i.second)               // second is parent
-        && !currentLimbIds.contains(i.first)
-        && !links2process.contains(i.first)
-        && !aForbiddenLinks.contains(id2name[i.first])) {  // first is child
-          auto jointType = aModel.getLink(id2name[i.first])->parent_joint->type;
+      for (auto i = linkId2parentLinkId.begin(); i != linkId2parentLinkId.end(); ++i) {                // O(n^2) but who cares? Not many links in an URDF.
+        if(currentLimbIds.contains(i->second)               // second is parent
+        && !currentLimbIds.contains(i->first)
+        && !links2process.contains(i->first)
+        && !aForbiddenLinks.contains(id2name[i->first])) {  // first is child
+          auto jointType = aModel.getLink(id2name[i->first])->parent_joint->type;
           if(jointType == urdf::Joint::FIXED) {
-            currentLimbIds.insert(i.first);
-            linkId2parentLinkId.emplace(i.first, actualLocalRootId);
-            mId2subRoot.emplace(i.first, actualLocalRootId);
+            currentLimbIds.insert(i->first);
+            mId2subRoot.emplace(i->first, actualLocalRootId);
             wasIncrement = true;
           }
           else if(jointType == urdf::Joint::PRISMATIC || jointType == urdf::Joint::REVOLUTE) {
-            links2process.insert(i.first);
+            links2process.insert(i->first);
           }
-          else { // Nothing to do, because we don't process anything else.
+          else { // We trim the parent tree here to avoid considering other joint types in further processing.
+            i = linkId2parentLinkId.erase(i);
           }
         }
         else { // nothing to do
--- src/EigenMeshModel.h
+++ src/EigenMeshModel.h
@@ -17,14 +17,18 @@ using Id = int32_t;
 class EigenMeshJointLink {
 private:
   struct ChildTransform {
-    std::shared_ptr<EigenMeshJointLink>  mChild;
-    Eigen::Matrix4f                      mTransform;
-  };
+    std::shared_ptr<EigenMeshJointLink> const mChild;
+    Eigen::Matrix4f const                     mTransform;        // Won't change since fixed joints lead to the child.
 
-  Id const                               mActualLocalRootId;
-  urdf::LinkConstSharedPtr               mFirstLink;
-  urdf::JointSharedPtr                   mJoint;
-  std::weak_ptr<EigenMeshJointLink>      mParent;                // Used to travel backwards for composition.
+    ChildTransform(std::shared_ptr<EigenMeshJointLink> const aChild, Eigen::Matrix4f const aTransform)
+    : mChild(aChild), mTransform(aTransform) {}
+  };
+  inline static constexpr char           csStlNamePrefix[] = "package://";
+
+  Id const                               mActualLocalRootId;     // +
+  urdf::LinkConstSharedPtr const         mFirstLink;             // +
+  urdf::JointSharedPtr     const         mJoint;                 // +
+  std::weak_ptr<EigenMeshJointLink>      mParent;                // + Used to travel backwards for composition.
   std::vector<Eigen::Vector3f>           mMesh;                  // Cumulative, in the mesh coordinate space of the local root link.
   std::unordered_map<Id, ChildTransform> mChildrenWithTransform; // All the cumulative transforms from the local root link of this mesh to the actual child's holding link.
   // These are identity for root.
@@ -38,7 +42,7 @@ public:
                      Id const aActualLocalRootId,
                      std::unordered_map<Id, Id> const &aLinkId2parentLinkId,
                      std::unordered_set<Id> const &aCurrentLimbIds,
-                     std::unordered_map<Id, std::string> const aId2name);
+                     std::unordered_map<Id, std::string> const &aId2name);
   void addChild(std::shared_ptr<EigenMeshJointLink> aChild);
   void addParent(std::shared_ptr<EigenMeshJointLink> aParent);
 };
